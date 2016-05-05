42d7b82f9215e2c54b9ebed5e8c1d9d364157ccc
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-05-05 18:29:01 +0900

    [solved] chapter3 32

---
+++ chapter4/a32.py
@@ -0,0 +1,25 @@
+import MeCab
+
+f = open('neko.txt')
+text = f.read()
+
+m = MeCab.Tagger ("-Ochasen -ucustom.dic")
+m.parse('')
+node =  m.parseToNode( text )
+
+class Morpheme:
+	def __init__(self, surface, base, pos, pos1):
+		self.surface = surface
+		self.base = base
+		self.pos  = pos
+		self.pos1 = pos1
+	
+tokens = []
+while node:
+	d = node.feature.split(',')
+	tokens.append(Morpheme(node.surface.encode('utf-8').decode('utf-8'),d[-3],d[0],d[1]))
+	node = node.next
+
+for token in tokens:
+	if "動詞" == token.pos:
+		print(token.base)
